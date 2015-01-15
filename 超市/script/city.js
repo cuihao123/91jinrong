203d57114b41198fe7ac11fa1a9f609cf453d227
cuihao <douke123@126.com>
2015-01-15 08:11:19 +0800

    更新

--- 超市/script/city.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$.getScript('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip=',function(){
-		var cityName = remote_ip_info.city;
-		$('.coordinate').html(cityName);
-	});
-});
